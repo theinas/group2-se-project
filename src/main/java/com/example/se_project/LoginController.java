654560959e76d912ed408d770087e09cf3ee7d2f
Inas, Hamad <hf3103@wayne.edu>
2022-11-30 14:57:23 -0500

    This commit is part of feature 2 Added the ability to add, delete, and update customers The search feature is yet to be implemented

--- src/main/java/com/example/se_project/LoginController.java
+++ src/main/java/com/example/se_project/LoginController.java
@@ -1,29 +1,113 @@
 package com.example.se_project;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class LoginController {
     @FXML
-    private Label welcomeText;
+    protected Label LoginText;
+    @FXML
+    protected ComboBox roleField;
+    @FXML
+    protected TextField usernameField;
+    @FXML
+    protected TextField passwordField;
+    @FXML
+    protected Button loginButton;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        LoginText.setText("Welcome to JavaFX Application!");
     }
 
    @FXML
-    private ComboBox<UserRoles> userRolesComboBox ;
-
-    @FXML
-    protected UserRoles getUserRolesComboBoxItems(){
-        for (UserRoles role: UserRoles.values())
+    public void onLoginButtonClick() throws SQLException, IOException {
+        UserRoles role = checkRole(roleField.getValue().toString());
+        String DBPassword = getPasswordFromDB(usernameField.getText(), passwordField.getText());
+        Boolean verified = verifyLogin(passwordField.getText(), DBPassword);
+        if (verified)
         {
-            return role;
+            LoginText.setText("Login Successful");
+            forwardUserToPage(role);
         }
-        return null;
-    }
-    
+
+        else
+            LoginText.setText("Incorrect username or password");
+
+
+
+   }
+   public void handleOwnerLogin() throws IOException {
+       Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Owner-view.fxml")));
+       Stage window = (Stage)loginButton.getScene().getWindow();
+       window.setScene(new Scene(root));
+   }
+   public void forwardUserToPage(UserRoles role) throws IOException {
+       switch (role)
+       {
+           case OWNER -> handleOwnerLogin();
+       }
+   }
+
+
+   public UserRoles checkRole(String r)
+   {
+       UserRoles role = null;
+       switch (r)
+       {
+           case "Admin":
+               role= UserRoles.ADMIN;
+               break;
+           case "Owner":
+               role=UserRoles.OWNER;
+               break;
+           case "Purchaser":
+               role = UserRoles.PURCHASER;
+               break;
+           case "Inventory Manager":
+               role = UserRoles.INVENTORY_MANAGER;
+               break;
+           case "Sales Person":
+               role = UserRoles.SALES_PERSON;
+               break;
+           case "Accountant":
+               role = UserRoles.ACCOUNTANT;
+               break;
+       }
+       return role;
+
+   }
+   public String getPasswordFromDB(String ID, String password) throws SQLException {
+       DBConnection connection = new DBConnection();
+       List<Map<String, Object>> results =  connection.getResults("select  password from users where id=" + ID);
+       connection.closeConnection();
+       return results.get(0).get("password").toString();
+   }
+
+   public boolean verifyLogin(String password, String DBpassword)
+   {
+       if (password.equals(DBpassword))
+       {
+           return true;
+       }
+       else
+       {
+           return false;
+       }
+   }
+
 
 }
